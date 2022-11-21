ad5acb0b85bb3e9081a40fafa14887fe378b91de
Ankur Srivastava <ankur@propertyguru.tech>
2022-11-21 15:13:15 +0530

    minor changes

--- index.test.js
+++ index.test.js
@@ -1,5 +1,23 @@
 import { sum, getData } from "./index"
 
+// Before all tests run
+beforeAll(() => {
+    // pre test run setup
+    // mock a service?
+})
+
+// After all tests run
+afterAll(() => {
+    // post test run setup
+    // clean up
+})
+
+//Before each test runs
+beforeEach(() => {})
+
+//After each test runs
+afterEach(() => {})
+
 test('adds 3 to 5 and return 8', () => {
     expect(sum(3, 5)).toBe(8)
 })
